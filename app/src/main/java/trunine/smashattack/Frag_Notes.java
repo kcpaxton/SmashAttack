c9cdc3c6ca7df7f86214c0afc87e18d880e0f54e
Kyle Paxton <kyle.paxton@jacks.sdstate.edu>
2018-01-27 10:44:20 -0600

    Switched buttons on the notes pages

--- app/src/main/java/trunine/smashattack/Frag_Notes.java
+++ app/src/main/java/trunine/smashattack/Frag_Notes.java
@@ -11,7 +11,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 public class Frag_Notes extends Fragment {
-
+    boolean selected =false;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -22,21 +22,37 @@ public class Frag_Notes extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        FloatingActionButton addNoteButton = getActivity().findViewById(R.id.createNoteFab);
+        final Button createNoteButton = getActivity().findViewById(R.id.createNoteButton);
 
         final FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
-
+        final FragmentTransaction ft2 = getActivity().getSupportFragmentManager().beginTransaction();
         final Fragment fragLeft = new Frag_Notes_Create_Select();
         final Fragment fragRight = new Frag_Notes_Create_Select();
+        final Fragment fragCreateNote = new Frag_Notes_Create();
 
-        addNoteButton.setOnClickListener(new View.OnClickListener() {
+        createNoteButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
+                if(selected)
+                {
+                    //remove fragLeft and fragRight and navigate to Frag_Notes_Create
+                    ft2.remove(fragLeft);
+                    ft2.remove(fragRight);
+                    ft2.replace(R.id.MyFrameLayout, fragCreateNote);
+                    ft2.commit();
+                    selected = false;
+                }
+                else
+                {
+                    ft.add(R.id.frag_left, fragLeft);
+                    ft.add(R.id.frag_right, fragRight);
+                    ft.commit();
+                    createNoteButton.setText("Next");
+                    selected = true;
+                }
 
-                ft.add(R.id.frag_left, fragLeft);
-                ft.add(R.id.frag_right, fragRight);
-                ft.commit();
+
             }
         });
     }
--- app/src/main/java/trunine/smashattack/Frag_Notes_Create.java
+++ app/src/main/java/trunine/smashattack/Frag_Notes_Create.java
@@ -6,13 +6,48 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.io.OutputStreamWriter;
 
 public class Frag_Notes_Create extends Fragment {
 
+    EditText editText;
+    ImageButton saveButton;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
         return inflater.inflate(R.layout.frag_notes_create, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        saveButton = (ImageButton) getActivity().findViewById(R.id.saveButton);
+        editText = (EditText) getActivity().findViewById(R.id.EditText1);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Save("Note1"); //Replace this name later
+            }
+        });
+    }
+
+
+    public void Save(String fileName) {
+        try {
+            OutputStreamWriter out =
+                    new OutputStreamWriter(getActivity().openFileOutput(fileName, 0));
+            out.write(editText.toString());
+            out.close();
+            Toast.makeText(getContext(), "Note Saved", Toast.LENGTH_SHORT).show();
+        } catch (Throwable t) {
+            Toast.makeText(getContext(), "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+        }
     }
 }
